8c58e54aac6f40a1ae9e43a33a96590225117aee
ExploitEngineer <revilwhoami0098@gmail.com>
2025-02-25 16:26:23 +0500

    Add folder 03_Interfaces&TypeAliases

---
+++ 03_Interfaces&TypeAliases/app.js
@@ -0,0 +1,17 @@
+function getDataOfUser(obj) {
+    return 0;
+}
+getDataOfUser({
+    name: "abdul rafay",
+    email: "newemail@gamil.com",
+    password: 1234
+});
+function abcd(obj) {
+    obj.admin;
+}
+function abcd1(obj) {
+}
+var car;
+function fun1(obj) {
+    console.log("hello");
+}
