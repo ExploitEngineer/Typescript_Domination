9e88b3488059129a82d18130c3ec3a97435489a8
ExploitEngineer <revilwhoami0098@gmail.com>
2025-02-25 14:47:30 +0500

    Add TypeInference & TypeAnnotations

---
+++ TypeInference_TypeAnnotations/app.js
@@ -0,0 +1,8 @@
+// Type Inference
+var num = 12;
+// Type annotations
+var newNum = 12;
+function newFun(a, b) {
+    console.log('hi');
+}
+newFun(12, "hello");
---
+++ TypeInference_TypeAnnotations/app.ts
@@ -0,0 +1,9 @@
+// Type Inference
+let num = 12;
+
+// Type annotations
+let newNum: number = 12;
+function newFun(a: number, b: string): void {
+    console.log('hi');
+}
+newFun(12, "hello");
