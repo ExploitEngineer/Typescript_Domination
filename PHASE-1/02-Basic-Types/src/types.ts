cf180fd860a2dd94042c9275c314992c43f33432
ExploitEngineer <revilwhoami0098@gmail.com>
2025-07-19 01:22:40 +0500

    feat: add TypeScript types
    - primitive types
    boolean
    number
    string
    void
    undefined
    null
    
    - object types
    interface
    class
    enum
    array
    tuple
    object
    
    - Top Types
    unknown
    any
    
    - Bottom Types
    never

--- PHASE-1/02-Basic-Types/src/types.ts
+++ PHASE-1/02-Basic-Types/src/types.ts
@@ -51,13 +51,64 @@ function doSomething(x: string | null) {
 // null
 let emptyValue: null = null;
 
-// any
-let randomData: any = 42;
-randomData = "hello";
-randomData = true;
-randomData.run(); // No error, even if run() doesn't exist
+// NOTE: 2. Object Types
 
-// unknown
+// Interface - Typescript allows you to specifically type an object using an interface that can be reused by multiple objects.
+interface Person {
+  name: string;
+  age: number;
+}
+
+function greet(person: Person) {
+  return `Hello ` + person.name;
+}
+
+// Class - In TypeScript, a class is a blueprint for creating objects with specific properties and methods. Classes are a fundamental concept in object-oriented programming.
+class Car {
+  make: string;
+  model: string;
+  year: number;
+
+  constructor(make: string, model: string, year: number) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+
+  drive() {
+    console.log(`Driving my ${this.year} ${this.make} ${this.model}`);
+  }
+}
+
+// Enum - Enums is not a type-level extension of JavaScript. It allows a developer to define a set of named constants. Using enums can make it easier to document intent, or create a set of distint cases. TypeScript provides both numeric and string-based enums.
+enum Direction {
+  Up = 1,
+  Down,
+  Left,
+  Right,
+}
+
+// Array - To specify the type of an array like [1, 2, 3], you can use the syntax number[]; this syntax works for any type (e.g. string[] is an array of strings, and so on). you may also see this written as Array<number>, which means the same thing.
+const numbers: number[] = [1, 2, 3];
+
+// Tuple - A tuple is another sort of Array type that knows exactly how many elements it contains, and exactly which types it contains at specific positions.
+type StringNumberPair = [string, number];
+const pair: StringNumberPair = ["Hello", 42];
+
+const first = pair[0];
+const second = pair[1];
+
+// Object - to define an object type, we simply list its properties and their types.
+// The parameter's type annotation is an object type
+function printCoord(pt: { x: number; y: number }) {
+  console.log("The coordinate's x value is " + pt.x);
+  console.log("The coordinate's of y value is " + pt.y);
+}
+printCoord({ x: 3, y: 7 });
+
+// NOTE: Top Types
+
+// UnKnown - unknown is the type-safe counterpart of any. Anything is assignable to unknown, but unknown isn't assignable to anything but itself and any without a type assertion or a control flow based narrowing. Likewise, no operations are premitted on an unknown without first asserting or narrowing to a more specific type.
 let inputValue: unknown = "test";
 inputValue = 123;
 inputValue = {};
@@ -67,11 +118,43 @@ if (typeof inputValue === "string") {
   console.log(inputValue.toUpperCase());
 }
 
-// never
+function f1(a: any) {
+  a.b(); // OK
+}
+
+function f2(a: unknown) {
+  // Error: property 'b' does not exist on type 'unknown'
+  // a.b();
+}
+
+// any
+let randomData: any = 42;
+randomData = "hello";
+randomData = true;
+randomData.run(); // No error, even if run() doesn't exist
+
+// NOTE: Bottom Types
+
+// Never - the never type represents the type of values that never occur. For instance, never is the return type for a function expression or an arrow function expression that always throws an exception or one that never returns. Variables also acquire the type never when narrowed by any type guards that can never be true. The never type is a subtype of, and assignable to, every type; however, no type is a subtype of, or assignable to, never (except never itself). Every any isn't assignable to never.
 function throwError(): never {
   throw new Error("This never returns");
 }
 
 function infiniteLoop(): never {
+  while (true) {}
+}
+
+// function returning never must not have a reachable end point
+function error(message: string): never {
+  throw new Error(message);
+}
+
+// Inferred return type is never
+function fail() {
+  return error("Something failed");
+}
+
+// Function returning never must not have a reachable end point
+function infiniteLoop1(): never {
   while (true) {}
 }
