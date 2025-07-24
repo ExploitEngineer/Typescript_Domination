44180d9ba03e05e8ff700ed359a2b8bc5426b023
ExploitEngineer <revilwhoami0098@gmail.com>
2025-07-25 04:03:36 +0500

    feat: add lesson-04
    add lesson-04 functions, type aliases

---
+++ Dave_Gray/Lesson-04/src/main.ts
@@ -0,0 +1,55 @@
+// Type Aliases
+
+type stringOrNumber = string | number;
+
+type stringOrNumberArray = (string | number)[];
+
+type Guitarist = {
+  name?: string;
+  active: boolean;
+  albums: stringOrNumber;
+};
+
+type UserId = stringOrNumber;
+
+// Literal Types
+let myName: "Dave";
+
+let userName: "Dave" | "John" | "Amy";
+userName = "Amy";
+
+// functions
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+const logMsg = (message: any): void => {
+  console.log(message);
+};
+
+logMsg("Hello!");
+logMsg(add(2, 3));
+
+let subtract = function (c: number, d: number): number {
+  return c - d;
+};
+
+type mathFunction = (a: number, b: number) => number;
+
+/* interface mathFunction {
+  (a: number, b: number): number;
+} */
+
+let multiply: mathFunction = function (c, d) {
+  return c * d;
+};
+
+logMsg(multiply(2, 2));
+
+// optional parameters
+const addAll = (a: number, b: number, c?: number): number => {
+  if (typeof c !== "undefined") {
+    return a + b + c;
+  }
+  return a + b;
+};
