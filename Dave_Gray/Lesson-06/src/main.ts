fa76afa958f8019a3ba3958beed36f40fa6bbda6
ExploitEngineer <revilwhoami0098@gmail.com>
2025-07-27 00:11:32 +0500

    refactor: Lesson-06 classes

--- Dave_Gray/Lesson-06/src/main.ts
+++ Dave_Gray/Lesson-06/src/main.ts
@@ -5,7 +5,7 @@ class Coder {
     public readonly name: string,
     public music: string,
     private age: number,
-    protected lang: string = "Typescript",
+    protected lang: string = "Typescript"
   ) {
     this.name = name;
     this.music = music;
@@ -28,7 +28,7 @@ class WebDev extends Coder {
     public computer: string,
     name: string,
     music: string,
-    age: number,
+    age: number
   ) {
     super(name, music, age);
     this.computer = computer;
@@ -68,3 +68,59 @@ class Guitarist implements Musician {
 
 const Page = new Guitarist("Jimmy", "guitar");
 console.log(Page.play("strums"));
+
+// *****************************************************
+
+class Peeps {
+  static count: number = 0;
+
+  static getCount(): number {
+    return Peeps.count;
+  }
+
+  public id: number;
+
+  constructor(public name: string) {
+    this.name = name;
+    this.id = ++Peeps.count;
+  }
+}
+
+const John = new Peeps("John");
+const Steve = new Peeps("Steve");
+const Amy = new Peeps("Amy");
+
+console.log(Amy.id);
+console.log(Steve.id);
+console.log(Peeps.count);
+
+// *****************************************************
+
+class Bands {
+  private dataState: string[];
+
+  constructor() {
+    this.dataState = [];
+  }
+
+  public get data(): string[] {
+    return this.dataState;
+  }
+
+  public set data(value: string[]) {
+    if (Array.isArray(value) && value.every((el) => typeof el === "string")) {
+      this.dataState = value;
+      return;
+    } else {
+      throw new Error("Param is not an array of strings");
+    }
+  }
+}
+
+const MyBands = new Bands();
+MyBands.data = ["Neil Young", "Led Zeppelin"];
+console.log(MyBands.data);
+MyBands.data = [...MyBands.data, "ZZ Top"];
+console.log(MyBands.data);
+console.log(MyBands.data);
+MyBands.data = ["Van Halen"];
