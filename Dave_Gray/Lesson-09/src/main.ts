e80ab59615f2f4ecafa35d312f61921db206b10f
ExploitEngineer <revilwhoami0098@gmail.com>
2025-07-28 23:34:31 +0500

    feat: add Utility Types
    - Partial
    - Required
    - Readonly
    - Record
    - Pick
    - Omit

---
+++ Dave_Gray/Lesson-09/src/main.ts
@@ -0,0 +1,83 @@
+// Utility Types
+
+// Partial
+
+interface Assignment {
+  studentId: string;
+  title: string;
+  grade: number;
+  verified?: boolean;
+}
+
+const updateAssignment = (
+  assign: Assignment,
+  propsToUpdate: Partial<Assignment>,
+): Assignment => {
+  return { ...assign, ...propsToUpdate };
+};
+
+const assign1: Assignment = {
+  studentId: "compsci123",
+  title: "Final Project",
+  grade: 0,
+};
+
+console.log(updateAssignment(assign1, { grade: 95 }));
+const assignGraded: Assignment = updateAssignment(assign1, { grade: 95 });
+
+// Required and Readonly
+
+const recordAssignment = (assign: Required<Assignment>): Assignment => {
+  // sends to database, etc.
+  return assign;
+};
+
+const assignVerified: Readonly<Assignment> = {
+  ...assignGraded,
+  verified: true,
+};
+
+// assignVerified.grade = 88;
+
+recordAssignment({ ...assignGraded, verified: true });
+
+// Record
+const hexColorMap: Record<string, string> = {
+  red: "FF0000",
+  green: "00FF00",
+  blue: "0000FF",
+};
+
+type Students = "Sara" | "Kelly";
+type LetterGrades = "A" | "B" | "C" | "D" | "U";
+
+const finalGrades: Record<Students, LetterGrades> = {
+  Sara: "B",
+  Kelly: "U",
+};
+
+interface Grades {
+  assign1: number;
+  assign2: number;
+}
+
+const gradeData: Record<Students, Grades> = {
+  Sara: { assign1: 85, assign2: 93 },
+  Kelly: { assign1: 76, assign2: 15 },
+};
+
+// Pick and Omit
+
+type AssignResult = Pick<Assignment, "studentId" | "grade">;
+
+const score: AssignResult = {
+  studentId: "K123",
+  grade: 85,
+};
+
+type AssignPreview = Omit<Assignment, "grade" | "verified">;
+
+const preview: AssignPreview = {
+  studentId: "k123",
+  title: "Final Project",
+};
