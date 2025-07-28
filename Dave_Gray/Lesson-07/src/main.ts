4efee40ee382fda624e1888d4a35077782db8670
ExploitEngineer <revilwhoami0098@gmail.com>
2025-07-28 21:44:30 +0500

    feat: add assertions using type

--- Dave_Gray/Lesson-07/src/main.ts
+++ Dave_Gray/Lesson-07/src/main.ts
@@ -70,3 +70,25 @@ const logStudentKey = (student: Student, key: keyof Student): void => {
 };
 
 logStudentKey(student, "GPA");
+
+////////////////////////////////////////////////////
+
+/*
+interface Incomes {
+  [key: string]: number;
+}
+*/
+
+type Streams = "salary" | "bonus" | "sidehustle";
+
+type Incomes = Record<Streams, number | string>;
+
+const monthlyIncomes: Incomes = {
+  salary: 500,
+  bonus: 100,
+  sidehustle: 250,
+};
+
+for (const revenue in monthlyIncomes) {
+  console.log(monthlyIncomes[revenue as keyof Incomes]);
+}
