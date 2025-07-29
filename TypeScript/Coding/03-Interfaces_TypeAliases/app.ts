14e04fbeea3bdf5d4bc2abbfbcd3e74fefbba24f
ExploitEngineer <revilwhoami0098@gmail.com>
2025-07-29 17:16:47 +0500

    feat: add union, intersection & types aliases

--- TypeScript/Coding/03-Interfaces_TypeAliases/app.ts
+++ TypeScript/Coding/03-Interfaces_TypeAliases/app.ts
@@ -20,3 +20,24 @@ function abcd(obj: Admin) {
   obj.admin = true;
   console.log(obj);
 }
+
+// Type Aliases
+type value = string | number | null;
+let a: value = "hi";
+
+// Union
+let b: string | null;
+
+// Intersection
+type Person = {
+  name: string;
+  email: string;
+};
+
+type Human = User & {
+  getDetails(user: string): void;
+};
+
+function func(a: Human): void {
+  console.log(a.name);
+}
